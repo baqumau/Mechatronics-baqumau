dc2ad24e46530a059ccd4e5677d3572721ef6f3e
Mauro Baquero-Suárez <baqumau@gmail.com>
2024-09-08 16:33:28 -0300

    Working on TI LAUNCHPAD F28377S for OMRs Controllers - Control_v003 -- saving 01

--- Robotics/Mobile_Robots/realApps/3WD_OMR_Formations/LAUNCHXL-F28377S/workspace_v12/Control_v003/main.c
+++ Robotics/Mobile_Robots/realApps/3WD_OMR_Formations/LAUNCHXL-F28377S/workspace_v12/Control_v003/main.c
@@ -13,19 +13,28 @@
 // $TI Release: F2837xS Support Library v130 $
 // $Release start: Wed May 22 of 2024 $
 //#######################################################################################################################
-// Defining configuration values:
+// Defining configuration macros:
 #define DEVICE_SYSCLK_FREQ 200000000
+#define BLINKY_LED_GPIO_01    12
+#define BLINKY_LED_GPIO_02    13
 //-----------------------------------------------------------------------------------------------------------------------
 // Including libraries to the main program:
 #include <math.h>
+#include <stdbool.h>
 #include <3WD_OMRs_Controllers.h>                                               // Controllers library.
 #include <3WD_OMRs_References.h>                                                // Reference library.
 #include <baqumau.h>                                                            // My library.
 #include "F28x_Project.h"                                                       // Device Header file and Examples Include File.
 //-----------------------------------------------------------------------------------------------------------------------
-// putting function declarations here:
-// Function to delay for a specified number of microseconds:
-void delay_us(unsigned long us);
+// Putting function declarations here:
+//-----------------------------------------------------------------------------------------------------------------------
+// Function to generate interrupt service through CPU timers 1, 2 and 3:
+__interrupt void cpu_timer0_isr(void);
+__interrupt void cpu_timer1_isr(void);
+__interrupt void cpu_timer2_isr(void);
+//-----------------------------------------------------------------------------------------------------------------------
+// Global variables:
+bool flagcommand_1 = false;                                                     // Setting flag command 1 to false.
 //-----------------------------------------------------------------------------------------------------------------------
 void main(void){
     // Step 1. Initialize System Control:
@@ -37,10 +46,14 @@ void main(void){
     // This example function is found in the F2837xS_Gpio.c file and
     // illustrates how to set the GPIO to it's default state.
     InitGpio();
-    GPIO_SetupPinMux(12, GPIO_MUX_CPU1, 0);
-    GPIO_SetupPinOptions(12, GPIO_OUTPUT, GPIO_PUSHPULL);
-    GPIO_SetupPinMux(13, GPIO_MUX_CPU1, 0);
-    GPIO_SetupPinOptions(13, GPIO_OUTPUT, GPIO_PUSHPULL);
+    // Configuring LED 01 and LED 02:
+    GPIO_SetupPinMux(BLINKY_LED_GPIO_01, GPIO_MUX_CPU1, 0);
+    GPIO_SetupPinOptions(BLINKY_LED_GPIO_01, GPIO_OUTPUT, GPIO_PUSHPULL);
+    GPIO_WritePin(BLINKY_LED_GPIO_01, 1);                                       // Turn LED GPIO 1 to OFF.
+    //---------------------------------------------------------
+    GPIO_SetupPinMux(BLINKY_LED_GPIO_02, GPIO_MUX_CPU1, 0);
+    GPIO_SetupPinOptions(BLINKY_LED_GPIO_02, GPIO_OUTPUT, GPIO_PUSHPULL);
+    GPIO_WritePin(BLINKY_LED_GPIO_02, 1);                                       // Turn LED GPIO 1 to OFF.
 
     // Step 3. Clear all interrupts and initialize PIE vector table:
     // Disable CPU interrupts
@@ -64,6 +77,43 @@ void main(void){
     // This function is found in F2837xS_PieVect.c.
     InitPieVectTable();
 
+    // Interrupts that are used in this example are re-mapped to
+    // ISR functions found within this file.
+    EALLOW;                                                                     // This is needed to write to EALLOW protected registers.
+    PieVectTable.TIMER0_INT = &cpu_timer0_isr;
+    PieVectTable.TIMER1_INT = &cpu_timer1_isr;
+    PieVectTable.TIMER2_INT = &cpu_timer2_isr;
+    EDIS;                                                                       // This is needed to disable write to EALLOW protected registers.
+
+    // Step 4. Initialize the Device Peripheral. This function can be
+    //         found in F2837xS_CpuTimers.c
+    InitCpuTimers();                                                            // Initialize the CPU Timers.
+
+    // Configure CPU-Timer 0, 1, and 2 to interrupt every second:
+    // 200MHz CPU Frequency, 1 second Period (in uSeconds)
+    ConfigCpuTimer(&CpuTimer0, 200, 1000000);
+    ConfigCpuTimer(&CpuTimer1, 200, 1000000);
+    ConfigCpuTimer(&CpuTimer2, 200, 1000000);
+
+    // To ensure precise timing, use write-only instructions to write to the entire register. Therefore, if any
+    // of the configuration bits are changed in ConfigCpuTimer and InitCpuTimers (in F2837xS_cputimervars.h), the
+    // below settings must also be updated.
+    CpuTimer0Regs.TCR.all = 0x4000;                                             // Use write-only instruction to set TSS bit = 0.
+    CpuTimer1Regs.TCR.all = 0x4000;                                             // Use write-only instruction to set TSS bit = 0.
+    CpuTimer2Regs.TCR.all = 0x4000;                                             // Use write-only instruction to set TSS bit = 0.
+
+    // Step 5. User specific code, enable interrupts:
+
+    // Enable CPU int1 which is connected to CPU-Timer 0, CPU int13
+    // which is connected to CPU-Timer 1, and CPU int 14, which is connected
+    // to CPU-Timer 2:
+    IER |= M_INT1;
+    IER |= M_INT13;
+    IER |= M_INT14;
+
+    // Enable TINT0 in the PIE: Group 1 interrupt 7:
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+
     // Enable global Interrupts and higher priority real-time debug events:
     EINT;  // Enable Global interrupt INTM
     ERTM;  // Enable Global real-time interrupt DBGM
@@ -71,35 +121,39 @@ void main(void){
     // Step 6. IDLE loop. Just sit and loop forever (optional):
     for(;;){
         //
-        // Turn on LED
-        //
-        GPIO_WritePin(12, 0);
-        GPIO_WritePin(13, 1);
-        //
-        // Delay for a bit.
-        //
-        delay_us(500000);
-        //
-        // Turn off LED
-        //
-        GPIO_WritePin(12, 1);
-        GPIO_WritePin(13, 0);
-        //
-        // Delay for a bit.
-        //
-        delay_us(500000);
+        NOP;                                                                    // No Operation (burn a cycle).
     }
 }
 //-----------------------------------------------------------------------------------------------------------------------
-// Function to delay for a specified number of microseconds:
-void delay_us(unsigned long us){
-    // Get the system clock frequency (e.g., 200 MHz):
-    unsigned long systemFrequencyMHz = DEVICE_SYSCLK_FREQ/1000000;              // Divide by 1 million to get MHz.
-    unsigned long cyclesNeeded = us*systemFrequencyMHz/125;                     // Calculate the required cycles.
-    //--------------------------------------------
-    // Simple loop-based delay:
-    while(cyclesNeeded > 0){
-        NOP;                                                                    // No Operation (burn a cycle).
-        cyclesNeeded--;
+// Putting function definitions here:
+//-----------------------------------------------------------------------------------------------------------------------
+__interrupt void cpu_timer0_isr(void){
+    CpuTimer0.InterruptCount++;
+
+    // Acknowledge this interrupt to receive more interrupts from group 1
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
+
+__interrupt void cpu_timer1_isr(void){
+    CpuTimer1.InterruptCount++;
+    // The CPU acknowledges the interrupt.
+
+}
+
+__interrupt void cpu_timer2_isr(void){
+    CpuTimer2.InterruptCount++;
+    // The CPU acknowledges the interrupt.
+    if(flagcommand_1){
+        GPIO_WritePin(BLINKY_LED_GPIO_01, 0);                                       // Turn LED GPIO 1 to ON.
+        GPIO_WritePin(BLINKY_LED_GPIO_02, 1);                                       // Turn LED GPIO 2 to OFF.
+        flagcommand_1 = false;                                                      // Set flag command 1 to FALSE.
+    }
+    else{
+        GPIO_WritePin(BLINKY_LED_GPIO_01, 1);                                       // Turn LED GPIO 1 to OFF.
+        GPIO_WritePin(BLINKY_LED_GPIO_02, 0);                                       // Turn LED GPIO 2 to ON.
+        flagcommand_1 = true;                                                       // Set flag command 1 to TRUE.
     }
 }
+//-----------------------------------------------------------------------------------------------------------------------
+// No more.
+//-----------------------------------------------------------------------------------------------------------------------
