32bd16d28a9daf703fc10f3241680f1af9de8820
Mauro Baquero-Suárez <baqumau@gmail.com>
2024-11-09 08:55:47 -0300

    Executing ADAMS-MATLAB co-Simulations of OMRs formation -- saving 012

--- Robotics/Mobile_Robots/3WD_OMR_Formations/Dynamics_and_Control-ADAMS_and_MATLAB/startUp_conSys_2robots_3_vR2014a.m
+++ Robotics/Mobile_Robots/3WD_OMR_Formations/Dynamics_and_Control-ADAMS_and_MATLAB/startUp_conSys_2robots_3_vR2014a.m
@@ -125,7 +125,7 @@ xc_0 = 10000; yc_0 = 10000; thc_0 = pi/2; d_0 = 2000;
 x1_0 = 12000; y1_0 = 10000; th1_0 = 0;
 x2_0 = 8000; y2_0 = 10000; th2_0 = 0;
 % With Cluster space references 1:
-dxc_0 = 0; dyc_0 = 0; dthc_0 = 0; dd_0 = 0; dph1_0 = .2*0; dph2_0 = -.2*0;
+dxc_0 = 0; dyc_0 = 0; dthc_0 = 0; dd_0 = 0; dph1_0 = .2; dph2_0 = -.2;
 o1r_0 = pi/2 - thc_0; o2r_0 = pi/2 - thc_0; do1r_0 = dthc_0 + dph1_0; do2r_0 = dthc_0 + dph2_0;
 %-----------------------------------
 dx1r_0 = dxc_0 + sin(thc_0)*dd_0 + d_0*cos(thc_0)*dthc_0;
