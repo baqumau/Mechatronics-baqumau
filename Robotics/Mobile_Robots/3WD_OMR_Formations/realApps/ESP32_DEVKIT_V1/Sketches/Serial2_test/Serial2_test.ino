940b16e19f681dd819a44ee83f19e848e0d5938b
Mauro Baquero-Suárez <baqumau@gmail.com>
2024-12-11 17:23:25 -0300

    Testing transmitted serial data to robots. Working on Serial2Test.ino of ESP32

--- Robotics/Mobile_Robots/3WD_OMR_Formations/realApps/ESP32_DEVKIT_V1/Sketches/Serial2_test/Serial2_test.ino
+++ Robotics/Mobile_Robots/3WD_OMR_Formations/realApps/ESP32_DEVKIT_V1/Sketches/Serial2_test/Serial2_test.ino
@@ -6,6 +6,7 @@
 #define Baud_Rate_2 115200                                                        // Baud rate value for UART 2 communication.
 //-----------------------------------------------------------------------------------
 // Including libraries to the program:
+#include <Arduino.h>
 #include <HardwareSerial.h>
 #include <string.h>
 #include <stdio.h>
@@ -17,23 +18,35 @@ HardwareSerial MySerial(1);                                                     
 // Defining the used variables in this sketch:
 const unsigned int RXD2 = 16;                                                     // Choosing this pin as UART 1 receiving data.
 const unsigned int TXD2 = 17;                                                     // Choosing this pin as UART 1 transmitting data.
+const unsigned int LED2 = 2;                                                      // Blue led of used ESP32.
 const unsigned int bufferSize_str = 64;                                           // buffer length for characters string.
 unsigned int long pulseTime = 0;                                                  // Variable to save pulse time when data is received by UART.
+unsigned int long count = 0;                                                      // Counter.
 char character_1;                                                                 // Variable to save received character by UART 0 module.
 char character_2;                                                                 // Variable to save received character by UART 2 module.
-hw_timer_t *Timer2_Cfg = NULL;                                                    // Pointer declaration for timer 2 execution.
+char measuredTime[24];                                                            // Char variable to save message about required time in the reception data via Serial 2 peripheral.   
+hw_timer_t *Timer2 = NULL;                                                        // Pointer declaration for timer 2 execution.
 bool flagcommand = false;                                                         // Required flag to count the time.
 //-----------------------------------------------------------------------------------
 // Putting function declarations here:
 void Timer2_Setup();                                                              // Declaration of Timer2_Setup function.
-void IRAM_ATTR Timer2_ISR();                                                      // Declaration of Timer2_ISR function.
+void IRAM_ATTR onTimer2_ISR();                                                    // Declaration of Timer2_ISR function.
 //-----------------------------------------------------------------------------------
 // Timer 2 interrupt at 10 Hz:
-void IRAM_ATTR Timer2_ISR(){
+void IRAM_ATTR onTimer2_ISR(){
   // Code for interrupt by Timer 2 is placed below:
+  digitalWrite(LED2,!digitalRead(LED2));                                          // Blink blue led.
+}
+//-----------------------------------------------------------------------------------
+// Setting Timer 2 and configuring its interruption function (at 10 Hz):
+void Timer2_Setup(){
+  Timer2 = timerBegin(10000000);                                                  // Set timer frequency to 10 MHz.
+  timerAttachInterrupt(Timer2,&onTimer2_ISR);                                     // Attaches the Timer 2 interrupt event with a desired ISR_Handler callback function (Timer2_ISR), that will be executed periodically.
+  timerAlarm(Timer2,1000000,true,0);                                              // Set alarm to call Timer2_ISR function every 0.1 seconds (value in microseconds). Repeat the alarm (third parameter) with unlimited count = 0 (fourth parameter).
 }
 //-----------------------------------------------------------------------------------
 void setup(){
+  pinMode(LED2,OUTPUT);                                                           // Defines GPIO 02 as output.
   //---------------------------------------------------------------------------------
   // Enabling UART communications:
   // Initialize serial monitor (UART 0):
@@ -46,13 +59,6 @@ void setup(){
   //---------------------------------------------------------------------------------
   // Enable Timer 2 interrupt at 10 Hz:
   Timer2_Setup();                                                                 // Call Timer2_setup function.
-}
-//-----------------------------------------------------------------------------------
-// Setting Timer 2 and configuring its interruption function (at 10 Hz):
-void Timer2_Setup(){
-  Timer2_Cfg = timerBegin(10);                                                    // Set timer frequency to 10 Hz.
-  timerAttachInterrupt(Timer2_Cfg,&Timer2_ISR);                                   // Attaches the Timer 2 interrupt event with a desired ISR_Handler callback function (Timer2_ISR), that will be executed periodically.
-  timerAlarm(Timer2_Cfg,100000,true,0);                                           // Set alarm to call Timer2_ISR function every 0.1 seconds (value in microseconds). Repeat the alarm (third parameter) with unlimited count = 0 (fourth parameter).
 }
 //-----------------------------------------------------------------------------------
 void loop(){
@@ -70,17 +76,13 @@ void loop(){
       character_2 = MySerial.read();                                              // Last received character from UART 0.
       Serial.print(character_2);                                                  // Print character 1 through UART 0.
       if(character_2 == '\n'){
-        delayMilliseconds(5);                                                     // Generate a 5 milliseconds delay.
-        flagcommand = false;                                                      
-        pulseTime = timerRead(Timer2_Cfg) - pulseTime;                            // Read the Compare Timer 2 register.
-        pulseTime = pulseTime/APB_CLK;                                            // Pulse time is translated from ticks to seconds.
-        Serial.print("Time: %1.4f",pulseTime);                                    // Print time of data reception.
+        pulseTime = timerRead(Timer2);                                            // Read the Compare Timer 2 register.
+        timerRestart(Timer2);                                                     // Restart timer 2.
+        float Time = 8.0f*pulseTime/APB_CLK;                                      // Pulse time is translated from ticks to seconds.
+        sprintf(measuredTime,"Time: %1.8f\n",Time);
+        Serial.print(measuredTime);                                               // Print time of data reception.
       }
     }
   }
   else NOP();                                                                     // No operation cycle.
-  if(!flagcommand){
-    pulseTime = timerRead(Timer2_Cfg);                                            // Read the Compare Timer 2 register.
-    flagcommand = true;                                                           // Turn flagcommand ON.
-  }
 }
