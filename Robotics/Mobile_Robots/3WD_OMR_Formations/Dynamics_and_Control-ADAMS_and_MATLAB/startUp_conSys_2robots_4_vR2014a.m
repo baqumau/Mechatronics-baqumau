880c9425fcfe355ea21f097de53ef488c49ecb7f
Mauro Baquero-Suárez <baqumau@gmail.com>
2025-04-07 16:01:48 -0300

    Changing SMC controller, from ADAMS-MATLAB simulations -- saving 06

--- Robotics/Mobile_Robots/3WD_OMR_Formations/Dynamics_and_Control-ADAMS_and_MATLAB/startUp_conSys_2robots_4_vR2014a.m
+++ Robotics/Mobile_Robots/3WD_OMR_Formations/Dynamics_and_Control-ADAMS_and_MATLAB/startUp_conSys_2robots_4_vR2014a.m
@@ -6,7 +6,7 @@ twoRobotsPlant_1_v2015
 % adams_sys
 % Loading control system 2 (SMC-CS):
 conSys_2robots_4_vR2014a
-Ts = 1/400;                                                                 %Sample time.
+Ts = 1/200;                                                                 %Sample time.
 %% Mechanical and Physical Properties:
 % Parameters of Omnibot 1:
 par.delta_1 = pi/6;                                                         %[rad], Wheels 1 and 2 are placed at this angle, with respect to y_m axis.
@@ -84,15 +84,15 @@ par.alpha_3 = diag([K1(1,4) K1(1,4) K2(1,4) K1(1,4) K2(1,4) K2(1,4)]);
 par.err = .42;
 %% Sliding mode control:
 % Tuning parameters:
-par.S_b = [180 180 19.5 180 19.5 19.5]';                                    % Constant for saturate sliding surfaces.
+par.S_b = [180 180 4*pi/3 150 pi pi]';                                      % Constant for saturate sliding surfaces.
 par.N = [55 55 35 55 35 35]';                                               % Constant of derivative filter.
 par.NTs = par.N*par.Ts;                                                     % Other constant of derivative filter.
-par.Eta = [1.84 1.84 1.84 1.84 1.84 1.84]';                                 % Constant for bounding the sliding surface dynamics.
+par.Eta = [1.84 1.84 1.96 1.84 6.14 6.14]';                                 % Constant for bounding the sliding surface dynamics.
 par.Delta = [1/4 1/20 1/20]';                                               % Constant for bounding the uncertainty model.
 par.Rho_1 = (1/8)*ones(3,1)*(par.m_1)*(par.l_1/par.r_1)^2;                  % Constant for bounding the input torque disturbnces in robot 1.
 par.Rho_2 = (1/8)*ones(3,1)*(par.m_2)*(par.l_2/par.r_2)^2;                  % Constant for bounding the input torque disturbnces in robot 2.
-par.Lambda = diag([2.68 2.68 2.68 2.68 2.68 2.68]);                         % Coefficients for sliding surface.
-par.dampFacts = diag([.81 .81 .81 .81 .81 .81]);                            % Assign damping factor to the sliding surfaces.
+par.Lambda = diag([1.8432 1.8432 1.8432 1.8432 2.7241 2.7241]);             % Coefficients for sliding surface.
+par.dampFacts = diag([1.15 1.15 1.15 1.15 1.01 1.01]);                      % Assign damping factor to the sliding surfaces.
 %% Extended Kalman Filters
 % EKF_1:
 par.b_1 = 87.5*0;                                                           % [mm] Offset for EKF (Position sensor of each vehicle in Adams model is located onto its rotation center).
